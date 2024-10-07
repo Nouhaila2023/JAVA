edcd30d867d4db2825e21bf5522866d14fafb59f
Nouhaila <148903525+Nouhaila2023@users.noreply.github.com>
2024-10-07 17:29:21 +0200

    Ejercicios de corso

--- Programacion/Ejercicios_Udemy/TipoStringCadenas/EjemploStringInmutable.java
+++ Programacion/Ejercicios_Udemy/TipoStringCadenas/EjemploStringInmutable.java
@@ -13,12 +13,18 @@ public class EjemploStringInmutable {
         System.out.println("Relacion = " + relacion);
         System.out.println(curso == relacion);
 
+///////////////////////////////////////////////////////////////////////////////
+
         String relacio2 = curso.transform((c) ->{
             return c + " con " + profesor;
         });
 
         System.out.println("relacion2 = " + relacio2);
 
+///////////////////////////////////////////////////////////////////////////////
+
+
+//.replace
         String relacion3 = relacion.replace("a" , "A");
         System.out.println("relacion3 = " + relacion3);
 
---
+++ Programacion/Ejercicios_Udemy/TipoStringCadenas/EjemploStringValidar.java
@@ -0,0 +1,17 @@
+package Ejercicios_Udemy.TipoStringCadenas;
+
+public class EjemploStringValidar {
+    public static void main(String[] args) {
+
+        String curso = null;
+        boolean esNulo = curso == null;
+
+        System.out.println("esNulo: " + esNulo);
+
+        if(esNulo == false){
+            System.out.println(curso.toUpperCase());
+        }
+        System.out.println();
+
+    }
+}
