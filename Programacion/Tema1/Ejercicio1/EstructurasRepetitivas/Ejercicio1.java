5f38448bbe3056438746bdc11d36a4742c5616c4
Nouhaila <148903525+Nouhaila2023@users.noreply.github.com>
2024-10-11 18:51:30 +0200

    Ejercicios practicos

--- Programacion/Tema1/Ejercicio1/EstructurasRepetitivas/Ejercicio1.java
+++ Programacion/Tema1/Ejercicio1/EstructurasRepetitivas/Ejercicio1.java
@@ -9,8 +9,36 @@ a adivinar es mayor o menor que el introducido, además de los intentos que te q
 muestra el número que había generado*/
 
 
+import java.util.Scanner;
+
 public class Ejercicio1 {
+
+
+    public static int numero_aleatorio(int min , int max){
+
+        return (int)(Math.random()*(max-min+1) +min);
+
+    }
+
     public static void main(String[] args) {
+
+        int numero = numero_aleatorio(1,100);
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 1; i <10 ; i++) {
+
+            int intregarNum = 0;
+            System.out.println("Intrega el numero:");
+            intregarNum = sc.nextInt();
+            if (intregarNum > numero){
+                System.out.println("Es Mayor");
+            }else if (intregarNum < numero){
+                System.out.println("Es Menor");
+            }else {
+                System.out.println("Es igual");
+            }
+
+        }
 
 
 
