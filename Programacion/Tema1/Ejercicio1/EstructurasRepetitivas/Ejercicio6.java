5a1eb1a0fc53ce6f2352e9788495afd88b5e058b
Nouhaila <148903525+Nouhaila2023@users.noreply.github.com>
2024-10-15 13:53:59 +0200

    Ejercicio Practico de casa

---
+++ Programacion/Tema1/Ejercicio1/EstructurasRepetitivas/Ejercicio6.java
@@ -0,0 +1,18 @@
+package Tema1.Ejercicio1.EstructurasRepetitivas;
+
+/*Ejercicio 6
+Una empresa tiene el registro de las horas que trabaja diariamente un empleado durante la
+semana (seis días) y requiere determinar el total de éstas, así como el sueldo que recibirá
+por las horas trabajadas.*/
+
+public class Ejercicio6 {
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+    }
+}
